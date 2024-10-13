e379b2878f4c33ff885e472eaa0e79f3c8209f62
Shivamtyaagi <shivamtyagishiva@gmail.com>
2024-10-13 21:06:46 +0400

    added some feature

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const image = [
+    "images/herosection_1.jpg",
+    "images/herosection_2.jpg",
+    "images/herosection_3.jpg",
+    "images/herosection_4.jpg",
+    "images/herosection_5.jpg",
+    "images/herosection_6.jpg"
+];
+
+
+let currentIndex = 0;
+
+function changeImage(){
+    const hero = document.getElementById('hero')
+    hero.classList.add('slide');
+    currentIndex = (currentIndex+1) %image.length;
+    hero.style.backgroundImage = `url(${image[currentIndex]})`;
+    setTimeout(() => {
+        hero.classList.remove('slide');
+        }, 1000);
+}
+
+setInterval(changeImage, 5000);
+
+let topButton = document.getElementById('backtotop')
+
+function toTop (){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
 
