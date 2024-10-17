17f4a0d7e557ad497ca13284552c21d98bfbe2c5
Shivamtyaagi <shivamtyagishiva@gmail.com>
2024-10-17 21:10:43 +0400

    modifications

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+
+
+
+window.onload = function() {
+    navigator.geolocation.getCurrentPosition(success, failed)
+
+};
+
+function success (position){
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    console.log(latitude,longitude)
+    getContryFromlocation(latitude, longitude)
+};
+
+function changeLoc (country){
+    const address = document.getElementById('add')
+    address.textContent = country;
+}
+
+
+function getContryFromlocation(latitude, longitude){
+    const api = 'cad60de3c57648fdafe8975eb58cc311';
+    fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${api}`)
+        .then(response => response.json())
+        .then(data => {
+            const country = data.results[0].components.country;
+            const countryCode = data.results[0].components.country_code.toUpperCase();
+            changeLoc(country);
+            flag(countryCode);
+        
+         })
+
+        .catch(error => console.log("error", error));
+}
+
+
+function failed (){
+    console.log("there was some error")
+    };
+
+
+
+function flag (countryCode){
+    const countryFlag = document.getElementById('flag')
+    countryFlag.src = `https://flagcdn.com/16x12/${countryCode}.png`;
+}   
+
+// hero secton image slider
 const image = [
     "images/herosection_1.jpg",
     "images/herosection_2.jpg",
@@ -10,10 +59,14 @@ const image = [
 
 let currentIndex = 0;
 
+const hero = document.getElementById('hero')
+
+
 function changeImage(){
-    const hero = document.getElementById('hero')
+    
     hero.classList.add('slide');
     currentIndex = (currentIndex+1) %image.length;
+    console.log(currentIndex)
     hero.style.backgroundImage = `url(${image[currentIndex]})`;
     setTimeout(() => {
         hero.classList.remove('slide');
@@ -22,6 +75,7 @@ function changeImage(){
 
 setInterval(changeImage, 5000);
 
+//move to Top button
 let topButton = document.getElementById('backtotop')
 
 function toTop (){
